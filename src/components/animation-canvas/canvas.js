1492d95418c7b7a6cfdaccad251541839defb5d0
Tom Yancey <thyancey84@gmail.com>
2021-03-20 23:37:25 -0400

    wip animation refactor

--- src/components/animation-canvas/canvas.js
+++ src/components/animation-canvas/canvas.js
@@ -5,26 +5,29 @@ import HookCanvas from './hook-canvas';
 export default class Canvas extends Component {
   constructor(props) {
     super(props);
-    this.saveContext = this.saveContext.bind(this);
+    this.saveCanvasContext = this.saveCanvasContext.bind(this);
   }
 
-  saveContext(ctx) {
+  saveCanvasContext(ctx) {
     this.ctx = ctx;
     this.width = this.ctx.canvas.width;
     this.height = this.ctx.canvas.height;
   }
 
+
   componentDidUpdate() {
-    this.props.drawCommands.forEach(dC => {
-      dC(this.ctx, { ...this.props });
-    })
+    if(this.ctx){
+      this.props.drawCommands.forEach(dC => {
+        dC(this.ctx, { ...this.props });
+      });
+    }
   }
 
   render() {
     // console.log('R: Canvas');
     return (
       <HookCanvas 
-        contextRef={this.saveContext} 
+        onSetCanvasRef={this.saveCanvasContext} 
         canvasWidth={this.props.canvasWidth} 
         canvasHeight={this.props.canvasHeight}
         onStageClick={this.props.onStageClick} />
--- src/components/animation-canvas/hook-canvas.js
+++ src/components/animation-canvas/hook-canvas.js
@@ -1,5 +1,5 @@
 /* based off of https://css-tricks.com/using-requestanimationframe-with-react-hooks/ */
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const useAnimationFrame = callback => {
   // Use useRef for mutable variables that we want to persist
@@ -24,22 +24,32 @@ const useAnimationFrame = callback => {
 }
 
 
-const HookCanvas = ({onStageClick, canvasWidth, canvasHeight, contextRef}) => {
-  const [count, setCount] = React.useState(0)
+const HookCanvas = ({onStageClick, canvasWidth, canvasHeight, onSetCanvasRef}) => {
+  const [count, setCount] = React.useState(0);
+  const [canvasRef, setCanvasRef] = React.useState(0);
+  
+  useEffect(() => {
+    if(canvasRef){
+      onSetCanvasRef(canvasRef);
+    }
+  }, [ onSetCanvasRef, canvasRef ]);
   
   useAnimationFrame(deltaTime => {
     // Pass on a function to the setter of the state
     // to make sure we always have the latest state
     setCount(prevCount => (prevCount + deltaTime * 0.01) % 100)
-  })
+  });
     
   return (
     <canvas
       onClick={onStageClick}
       width={canvasWidth}
       height={canvasHeight}
+      // ref={node =>
+      //   node ? contextRef(node.getContext('2d')) : null
+      // }
       ref={node =>
-        node ? contextRef(node.getContext('2d')) : null
+        node ? setCanvasRef(node.getContext('2d')) : null
       }
     />
   );
